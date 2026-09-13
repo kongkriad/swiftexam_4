a0facf0715b4daa6bff3cd8f69c4ca992f51b9ae
kong <tee0918831415@gmail.com>
2026-09-14 00:35:44 +0700

    v1 school

---
+++ apis/views/v1/school.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+
+from apis.models import School
+from apis.serializers import SchoolSerializer, SchoolDetailSerializer
+from apis.filters import SchoolFilter
+
+class ScoolViewSet(viewsets.ModelViewSet):
+    queryset = School.objects.all()
+    serializer_class = SchoolSerializer
+    filterset_class = SchoolFilter
+
+    def get_serializer_class(self):
+        if self.action == 'retrieve':
+            return SchoolDetailSerializer
+        return SchoolSerializer
