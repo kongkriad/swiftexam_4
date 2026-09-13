6b9d4bdd1efa0b8c76144ee1f30b59a63a90a2e2
kong <tee0918831415@gmail.com>
2026-09-14 01:29:31 +0700

    updata serializers

--- apis/serializers.py
+++ apis/serializers.py
@@ -2,30 +2,27 @@ from rest_framework import serializers
 
 from apis.models import School,Classroom,Teacher,Student
 
-class TeacherSimpleSerializer(serializers.ModelSerializer):
+class ClassroomMiniSerializer(serializers.ModelSerializer):
+    school_name = serializers.CharField(source='school.name', read_only=True)
+
+    class Meta:
+        model =Classroom
+        fields = ['id', 'school', 'school_name', 'grade_level', 'room_number']
+
+class TeacherMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = ['id', 'first_name', 'last_name', 'gender']
 
-
-class StudentSimpleSerializer(serializers.ModelSerializer):
+class StudentMiniSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'first_name', 'last_name', 'gender']
-
-
-class ClassroomSimpleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Classroom
-        fields = ['id', 'year', 'room']
-
-# ---------- School ----------
 
 class SchoolSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = ['id', 'name', 'abbreviation', 'address']
-
 
 class SchoolDetailSerializer(serializers.ModelSerializer):
     classroom_count = serializers.SerializerMethodField()
@@ -36,63 +33,50 @@ class SchoolDetailSerializer(serializers.ModelSerializer):
         model = School
         fields = [
             'id', 'name', 'abbreviation', 'address',
-            'classroom_count', 'teacher_count', 'student_count'
+            'classroom_count', 'teacher_count', 'student_count',
         ]
 
     def get_classroom_count(self, obj):
         return obj.classrooms.count()
 
     def get_teacher_count(self, obj):
-        # นับครูที่สอนในห้องเรียนของโรงเรียนนี้ (ไม่ซ้ำ)
         return Teacher.objects.filter(classrooms__school=obj).distinct().count()
 
     def get_student_count(self, obj):
         return Student.objects.filter(classroom__school=obj).count()
 
-
-# ---------- Classroom ----------
-
 class ClassroomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Classroom
-        fields = ['id', 'school', 'year', 'room']
-
+        fields = ['id', 'school', 'grade_level', 'room_number']
 
 class ClassroomDetailSerializer(serializers.ModelSerializer):
-    teachers = TeacherSimpleSerializer(many=True, read_only=True)
-    students = StudentSimpleSerializer(many=True, read_only=True)
+    teachers = TeacherMiniSerializer(many=True, read_only=True)
+    students = StudentMiniSerializer(many=True, read_only=True)
 
     class Meta:
-        model = Classroom
-        fields = ['id', 'school', 'year', 'room', 'teachers', 'students']
-
-
-# ---------- Teacher ----------
+        modl = Classroom
+        fields = ['id', 'school', 'grade_level', 'room_number', 'teachers', 'students']
 
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = ['id', 'first_name', 'last_name', 'gender', 'classrooms']
 
-
 class TeacherDetailSerializer(serializers.ModelSerializer):
-    classrooms = ClassroomSimpleSerializer(many=True, read_only=True)
+        classrooms = ClassroomMiniSerializer(many=True, read_only=True)
 
-    class Meta:
-        model = Teacher
-        fields = ['id', 'first_name', 'last_name', 'gender', 'classrooms']
-
-
-# ---------- Student ----------
+        class Meta:
+            model = Teacher
+            fields = ['id', 'first_name', 'last_name', 'gender', 'classrooms']
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = ['id', 'first_name', 'last_name', 'gender', 'classroom']
 
-
 class StudentDetailSerializer(serializers.ModelSerializer):
-    classroom = ClassroomSimpleSerializer(read_only=True)
+    classroom = ClassroomMiniSerializer(read_only=True)
 
     class Meta:
         model = Student
