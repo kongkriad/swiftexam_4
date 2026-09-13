e394da6d4410ddf8dba8f31454977c32c96f716d
kong <tee0918831415@gmail.com>
2026-09-13 23:47:38 +0700

    admin

--- apis/admin.py
+++ apis/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from apis.models import School,Classroom,Teacher,Student
+
+admin.site.register(School)
+admin.site.register(Classroom)
+admin.site.register(Teacher)
+admin.site.register(Student)
 # Register your models here.
