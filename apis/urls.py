ce4f619d483b9817bffba6c0e03a4bcf56b3df68
kong <tee0918831415@gmail.com>
2026-09-14 01:34:39 +0700

    url

--- apis/urls.py
+++ apis/urls.py
@@ -1,8 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-
+from apis.views.v1 import(
+    SchoolViewSet,
+    ClassroomViewSet,
+    TeacherViewSet,
+    StudentViewSet,
+)
 router = DefaultRouter()
+router.register('schools',SchoolViewSet,basename='school')
+router.register('classrooms',ClassroomViewSet,basename='classroom')
+router.register('teachers',TeacherViewSet,basename='teacher')
+router.register('students',StudentViewSet,basename='student')
 
 api_v1_urls = (router.urls, 'v1')
 
